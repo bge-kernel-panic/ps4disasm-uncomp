4af605888fb299bee7701b852849333d9b84203a
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2018-03-09 20:20:51 -0500

    Further translations.

--- script/dialogue 16.asm
+++ script/dialogue 16.asm
@@ -121,95 +121,99 @@
 ; $28
 	dc.b	$F4
 	dc.b	$09
-	dc.b	"Gimme a drink, gimme a drink!"
+	dc.b	"À boire! À boire!"
 	dc.b	$FC
-	dc.b	"Ha, ha, ha!"
+	dc.b	"Ha ha ha!"
 	dc.b	$FF
 
 ; $29
 	dc.b	$F4
 	dc.b	$09
-	dc.b	"Yes. Good drink and good food."
+	dc.b	"Ouais! À boire et à manger."
 	dc.b	$FC
-	dc.b	"This is heaven."
+	dc.b	"Ça, c'est la vie."
 	dc.b	$FF
 
 ; $2A
-	dc.b	"People from all over Dezolis"
+	dc.b	"Les gens viennent ici des"
 	dc.b	$FC
-	dc.b	"come here and talk of all sorts"
+	dc.b	"quatres coins de Dezolis et"
 	dc.b	$FD
-	dc.b	"of things... "
+	dc.b	"racontent plein de choses... "
 	dc.b	$FD
-	dc.b	"Gyuna listens to all this and"
+	dc.b	"Gyuna les écoutent tous et"
 	dc.b	$FC
-	dc.b	"passes the information on..."
+	dc.b	"transmet cela aux autres..."
 	dc.b	$FD
-	dc.b	"This is where peoples' ideas"
+	dc.b	"C'est ici que toutes les idées"
 	dc.b	$FC
-	dc.b	"gather and are disseminated."
+	dc.b	"se rassemblent et diffusent."
 	dc.b	$FF
 
 ; $2B
-	dc.b	"The owner here, Gyuna, is very"
+	dc.b	"Le propriétaire de cet établis-"
 	dc.b	$FC
-	dc.b	"knowledgeable, but he has a"
+	dc.b	"sement, Gyuna, connaît son"
 	dc.b	$FD
-	dc.b	"terribly old-fashioned accent!"
+	dc.b	"travail, mais il a un accent"
+	dc.b	$FC
+	dc.b	"terriblement ringard!"
 	dc.b	$FF
 
 ; $2C
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"I heard they have really good"
+	dc.b	"On m'avait vanté la qualité des"
 	dc.b	$FC
-	dc.b	"drinks here, so I traveled far"
+	dc.b	"boissons ici, c'est pourquoi"
 	dc.b	$FD
-	dc.b	"to come here... "
+	dc.b	"je suis venu de loin..."
 	dc.b	$FD
-	dc.b	"But the snow storm has blocked"
+	dc.b	"Mais je suis maintenant bloqué"
 	dc.b	$FC
-	dc.b	"the roads with ice and I can't"
+	dc.b	"ici à cause de la glace qui"
 	dc.b	$FD
-	dc.b	"get home."
+	dc.b	"obstrue les chemins."
 	dc.b	$FF
 
 ; $2D
-	dc.b	"I heard they have really good"
+	dc.b	"On m'avait vanté la qualité des"
 	dc.b	$FC
-	dc.b	"drinks here, so I traveled far"
+	dc.b	"boissons ici, c'est pourquoi"
 	dc.b	$FD
-	dc.b	"to come here..."
+	dc.b	"je suis venu de loin..."
 	dc.b	$FD
-	dc.b	"But the ice won't melt even"
+	dc.b	"Mais la glace n'a toujours pas"
 	dc.b	$FC
-	dc.b	"though the snow storm has"
+	dc.b	"fondue malgré que la tempête"
 	dc.b	$FD
-	dc.b	"subsided and I can't get home."
+	dc.b	"se soit calmée, et je suis"
+	dc.b	$FC
+	dc.b	"bloqué ici..."
 	dc.b	$FF
 
 ; $2E
-	dc.b	"Aiee! What are you doing?"
+	dc.b	"Aiee! Que me voulez-vous?"
 	dc.b	$FC
-	dc.b	"Boy, you're a suspicious-"
+	dc.b	"Wow, vous avez vraiment une"
 	dc.b	$FD
-	dc.b	"looking lot."
+	dc.b	"allure douteuse."
 	dc.b	$FD
-	dc.b	"You're making my drink go sour."
+	dc.b	"Vous faites tourner ma boisson."
 	dc.b	$FC
-	dc.b	"Don't get too close."
+	dc.b	"Éloignez-vous de moi!"
 	dc.b	$FF
 
 ; $2F
-	dc.b	"Waaaaaa. I...I was wrong."
+	dc.b	"Ouiiin. J'a...j'avais tort!"
 	dc.b	$FC
-	dc.b	"Please come back."
+	dc.b	"Revenez, je vous en supplie!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"He's a maudlin drunk..."
+	dc.b	"Il a le vin triste... C'est"
 	dc.b	$FC
-	dc.b	"What a pain."
+	dc.b	"vraiment pénible."
 	dc.b	$FF
 
 ; $30
@@ -222,19 +226,19 @@
 	dc.b	$81, $01
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"Oh my, a first-time customer."
+	dc.b	"Oh, heille, des nouveaux"
 	dc.b	$FC
-	dc.b	"Ah, you're friends of Raja."
+	dc.b	"clients! Ah, z'êtes des amis"
 	dc.b	$FD
-	dc.b	"In that case,"
+	dc.b	"de Raja! Bon, alors, je vais"
 	dc.b	$FC
-	dc.b	"I'll tell you anything."
+	dc.b	"pas faire de cachettes."
 	dc.b	$FD
-	dc.b	"What do you want to know?"
+	dc.b	"Qu'est-ce vous voulez savoir?"
 	dc.b	$FD
-	dc.b	"Do you want to know about the"
+	dc.b	"Z'êtes curieux des dommages"
 	dc.b	$FC
-	dc.b	"damage from the snow storm?"
+	dc.b	"amenés par la tempête?"
 	dc.b	$F5
 	dc.b	$02, $04
 	dc.b	$FF
@@ -242,15 +246,15 @@
 ; $32
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"Oh, you're friends of Raja."
-	dc.b	$FC
-	dc.b	"Do you want to know about"
+	dc.b	"Ah, z'êtes les amis de Raja."
 	dc.b	$FD
-	dc.b	"something else?"
-	dc.b	$FD
-	dc.b	"Do you want to now about the"
+	dc.b	"Voulez savoir qu'èque chose"
 	dc.b	$FC
-	dc.b	"damage from the snow storm?"
+	dc.b	"d'autre?"
+	dc.b	$FD
+	dc.b	"Z'êtes curieux des dommages"
+	dc.b	$FC
+	dc.b	"amenés par la tempête?"
 	dc.b	$F5
 	dc.b	$01, $03
 	dc.b	$FF
@@ -258,182 +262,188 @@
 ; $33
 	dc.b	$FA
 	dc.b	$A1, $01
-	dc.b	"This snow storm started three"
+	dc.b	"C'te tempête a commencée y'a"
 	dc.b	$FC
-	dc.b	"months ago, and hasn't stopped"
+	dc.b	"trois mois, et elle a pas"
 	dc.b	$FD
-	dc.b	"since."
+	dc.b	"arrêtée depuis."
 	dc.b	$FD
-	dc.b	"It's been a terrible disaster,"
+	dc.b	"C'est une affaire terrible, des"
 	dc.b	$FC
-	dc.b	"hats being blown off all over"
+	dc.b	"chapeaux qui volent partout pis"
 	dc.b	$FD
-	dc.b	"the place and lips getting"
+	dc.b	"des lèvres qui gèrcent à vue"
 	dc.b	$FC
-	dc.b	"chapped!"
+	dc.b	"d'oeil!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Hats? Lips?"
+	dc.b	"... quelle importance?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"Ahem! But, the biggest problem"
+	dc.b	"Hum! Mais le gros trouble, c'est"
 	dc.b	$FC
-	dc.b	"is the wall of ice!"
+	dc.b	"c'te mur de glace!"
 	dc.b	$FD
-	dc.b	"The storm has left snow drifts"
+	dc.b	"La tempête a laissé plein de"
 	dc.b	$FC
-	dc.b	"all over the place, and has"
+	dc.b	"bancs de neige, pis ça a toutte"
 	dc.b	$FD
-	dc.b	"created walls of ice."
+	dc.b	"compacté en glace."
 	dc.b	$FD
-	dc.b	"This has divided Dezolis up"
+	dc.b	"Là, Desolis est toutte séparée"
 	dc.b	$FC
-	dc.b	"into parts."
+	dc.b	"en morceaux."
 	dc.b	$FD
-	dc.b	"We can't get anywhere!"
+	dc.b	"On peut pû aller nulle part!"
 	dc.b	$FF
 
 ; $34
-	dc.b	"I don't know if it was the call"
+	dc.b	"16-34 Je ne sais pas si c'est"
 	dc.b	$FC
-	dc.b	"of the heavens or the screams"
+	dc.b	"l'intervention divine ou les"
 	dc.b	$FD
-	dc.b	"of the earth, but suddenly the"
+	dc.b	"pleurs de la terre, mais la"
 	dc.b	$FC
-	dc.b	"snow storm stopped."
+	dc.b	"tempête s'est soudainement"
 	dc.b	$FD
-	dc.b	"But according to stories I've"
-	dc.b	$FC
-	dc.b	"heard, Gumbious Temple has... "
+	dc.b	"calmée."
 	dc.b	$FD
-	dc.b	"No, no!"
+	dc.b	"Mais j'ai entendu des rumeurs"
 	dc.b	$FC
-	dc.b	"It can't be true."
+	dc.b	"à propos du temple Gumbious..."
+	dc.b	$FD
+	dc.b	"Non, c'est impossible!"
+	dc.b	$FC
+	dc.b	"Ce doit être des mensonges."
 	dc.b	$FF
 
 ; $35
-	dc.b	"Do you want to know about the"
+	dc.b	"Vous voulez savoir de quoi sur"
 	dc.b	$FC
-	dc.b	"Garuberk Tower?"
+	dc.b	"la Tour Garuberk?"
 	dc.b	$F5
 	dc.b	$00, $03
 	dc.b	$FA
 	dc.b	$A1, $02
 	dc.b	$FA
 	dc.b	$94, $01
-	dc.b	"It's said there's a tower"
+	dc.b	"Apparament qu'y a une tour"
 	dc.b	$FC
-	dc.b	"somewhere in Dezolis where"
+	dc.b	"que'que part sur Dezolis"
 	dc.b	$FD
-	dc.b	"the demons live."
+	dc.b	"infestée de démons!"
 	dc.b	$FD
-	dc.b	"As the story goes, the tower"
+	dc.b	"En plus, ça a l'air que la Tour"
 	dc.b	$FC
-	dc.b	"suddenly appeared overnight!"
+	dc.b	"est apparue drette en une nuit!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$09
-	dc.b	"That's it! It's those demons"
+	dc.b	"Oui, c'est ce que j'ai dit! Ce"
 	dc.b	$FC
-	dc.b	"who are destroying the universe!"
+	dc.b	"sont les démons qui veulent"
 	dc.b	$FD
-	dc.b	"Algo is under a curse!"
+	dc.b	"détruite l'univers! Ils ont"
+	dc.b	$FC
+	dc.b	"jeté un mauvais sort sur Algo!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"Could what Raja says be true?"
+	dc.b	"Est-il possible que Raja ait"
+	dc.b	$FC
+	dc.b	"raison?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"That I don't know."
+	dc.b	"Va savoir!"
 	dc.b	$FD
-	dc.b	"But, Raja being what he is,"
+	dc.b	"Mais, Raja c'est Raja, ça se"
 	dc.b	$FC
-	dc.b	"he may be sensing something."
+	dc.b	"peut qu'il sente des choses."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"And you have no idea at all"
+	dc.b	"Et vous n'avez aucune idée d'où"
 	dc.b	$FC
-	dc.b	"where the Garuberk Tower is?"
+	dc.b	"se trouve la Tour Garuberk?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"All who've seen it say"
+	dc.b	"Bah, tout le monde me dit des"
 	dc.b	$FC
-	dc.b	"something different."
+	dc.b	"choses différentes."
 	dc.b	$FD
-	dc.b	"...All I can say is that it's"
+	dc.b	"...Tout ce que je peux dire,"
 	dc.b	$FC
-	dc.b	"up north somewhere. "
+	dc.b	"c'est qu'elle est que'que part"
+	dc.b	$FD
+	dc.b	"vers le nord."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"I see..."
+	dc.b	"Je vois..."
 	dc.b	$FF
 
 ; $36
-	dc.b	"It's said there's a tower"
+	dc.b	"Y'a des rumeurs qu'il y a une"
 	dc.b	$FC
-	dc.b	"somewhere in Dezolis where"
+	dc.b	"tour que'que part sur Dezolis"
 	dc.b	$FD
-	dc.b	"the demons live."
+	dc.b	"qui est infestée de démons."
 	dc.b	$FD
-	dc.b	"As the story goes, the tower"
+	dc.b	"Apparamment qu'elle est apparue"
 	dc.b	$FC
-	dc.b	"suddenly appeared overnight!"
+	dc.b	"drette de même en une nuit!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"And you have no idea at all"
+	dc.b	"Et vous n'avez vraiment aucune"
 	dc.b	$FC
-	dc.b	"where the Garuberk Tower is?"
+	dc.b	"idée où la Tour Garuberk se"
+	dc.b	$FD
+	dc.b	"trouve?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"I didn't say that."
+	dc.b	"C'est pas c'que j'ai dit."
 	dc.b	$FD
-	dc.b	"I have a feeling it's north of"
+	dc.b	"J'ai l'impression qu'elle est"
 	dc.b	$FC
-	dc.b	"Meese."
+	dc.b	"que'que part au nord de Meese."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"I see."
+	dc.b	"Je vois."
 	dc.b	$FF
 
 ; $37
-	dc.b	"But, what d'you know,"
+	dc.b	"16-37 C'est bizarre, ça a l'air"
 	dc.b	$FC
-	dc.b	"apparently the Garuberk Tower"
+	dc.b	"que la Tour Garuberk a disparu!"
 	dc.b	$FD
-	dc.b	"has disappeared."
-	dc.b	$FD
-	dc.b	"And it's because of its"
+	dc.b	"Pis en plus, que c'est à cause"
 	dc.b	$FC
-	dc.b	"disappearance that this snow"
+	dc.b	"de ça que la tempête est calmée."
 	dc.b	$FD
-	dc.b	"storm has subsided."
-	dc.b	$FD
-	dc.b	"It's said that some"
+	dc.b	"Ouais, pis ça a l'air qu'un gars"
 	dc.b	$FC
-	dc.b	"macho-looking hero"
+	dc.b	"ben smatte pis sa gang ont foutu"
 	dc.b	$FD
-	dc.b	"took along his followers and"
+	dc.b	"la trouille aux démons pis ont"
 	dc.b	$FC
-	dc.b	"destroyed the evil tower."
+	dc.b	"tout cassé la baraque."
 	dc.b	$FD
-	dc.b	"Who could that have been?"
+	dc.b	"J'me d'mande ben qui c'était?"
 	dc.b	$FC
-	dc.b	"I can't think of anyone who"
+	dc.b	"Y'a pas personne que j'connais"
 	dc.b	$FD
-	dc.b	"would fit that description."
+	dc.b	"qui fitte c'te portrait-là."
 	dc.b	$FF
 
 ; $38
-	dc.b	"You want to know about Raja?"
+	dc.b	"Voulez en savoir plus sur Raja?"
 	dc.b	$F5
 	dc.b	$00, $04
 	dc.b	$FA
@@ -442,158 +452,152 @@
 	dc.b	$A1, $02
 	dc.b	$FA
 	dc.b	$94, $01
-	dc.b	"I've known him a long time."
+	dc.b	"J'le connais depuis un boutte."
 	dc.b	$FC
-	dc.b	"He's a weird one...but,"
+	dc.b	"Yé bizarre... mais y connait son"
 	dc.b	$FD
-	dc.b	"he's a skilled magician,"
+	dc.b	"affaire en magie, pis yé ben"
 	dc.b	$FC
-	dc.b	"and he was well liked at"
-	dc.b	$FD
-	dc.b	"Gumbious Temple."
+	dc.b	"apprécié au Temple Gumbious."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Wow."
+	dc.b	"Wow. Je n'arrive pas a y croire."
 	dc.b	$FC
-	dc.b	"I can't believe it."
-	dc.b	$FD
-	dc.b	"That old freak!?"
+	dc.b	"Ce vieux bizarre!?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"But, he was tricked by those"
+	dc.b	"Mais ya des gens qui étaient"
 	dc.b	$FC
-	dc.b	"who were jealous of him,"
+	dc.b	"jaloux qui lui ont passé un"
 	dc.b	$FD
-	dc.b	"and was banished to this rural"
+	dc.b	"sapin, pis ya été banni au"
 	dc.b	$FC
-	dc.b	"place."
+	dc.b	"boutte du monde."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Really... "
+	dc.b	"Vraiment..."
 	dc.b	$FC
-	dc.b	"It doesn't seem possible."
+	dc.b	"Ça ne m'apparait pas plausible."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"Actually Raja himself is quite"
+	dc.b	"Comme de faite, Raja est ben"
 	dc.b	$FC
-	dc.b	"happy that he was able to get"
+	dc.b	"content de pouvoir se débarasser"
 	dc.b	$FD
-	dc.b	"away from that constrained"
-	dc.b	$FC
-	dc.b	"temple!"
+	dc.b	"de ce temple ben trop guindé!"
 	dc.b	$FD
-	dc.b	"He's a good guy!"
+	dc.b	"C't'un bon gars! Yé vraiment"
 	dc.b	$FC
-	dc.b	"He's a good friend of mine!"
+	dc.b	"un bon ami!"
 	dc.b	$FF
 
 ; $39
-	dc.b	"Raja has fallen? But he's the"
+	dc.b	"16-39 Raja est malade? Mais"
 	dc.b	$FC
-	dc.b	"kind of guy who would return"
+	dc.b	"c'est l'genre de gars qui va"
 	dc.b	$FD
-	dc.b	"even if you killed him."
+	dc.b	"se relever même si on le tue."
 	dc.b	$FC
-	dc.b	"...Still, this is kind of"
+	dc.b	"...Ouais, ça m'inquiète quand"
 	dc.b	$FD
-	dc.b	"worrisome..."
+	dc.b	"même..."
 	dc.b	$FF
 
 ; $3A
-	dc.b	"Raja has fallen, and according"
+	dc.b	"19-3A Raja est malade, pis"
 	dc.b	$FC
-	dc.b	"to rumors,"
+	dc.b	"ça a l'air que le Temple"
 	dc.b	$FD
-	dc.b	"Gumbious Temple has..."
-	dc.b	$FC
-	dc.b	"No, that must be a lie."
+	dc.b	"Gumbious a été..."
 	dc.b	$FD
-	dc.b	"Why do all these horrible"
+	dc.b	"Non, ça, ça doit être des"
 	dc.b	$FC
-	dc.b	"things happen?"
+	dc.b	"menteries."
+	dc.b	$FD
+	dc.b	"Pourquoi y'a tant de trucs"
+	dc.b	$FC
+	dc.b	"horrible qui s'passent?"
 	dc.b	$FF
 
 ; $3B
-	dc.b	"I've known him a long time."
+	dc.b	"J'le connais depuis un boutte."
 	dc.b	$FC
-	dc.b	"He's a weird one...but,"
+	dc.b	"Yé bizarre... mais y connait son"
 	dc.b	$FD
-	dc.b	"he's a skilled magician,"
+	dc.b	"affaire en magie, pis yé ben"
 	dc.b	$FC
-	dc.b	"and he was well liked at"
-	dc.b	$FD
-	dc.b	"'Gumbious Temple.'"
+	dc.b	"apprécié au Temple Gumbious."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Wow."
+	dc.b	"Wow. Je n'arrive pas a y croire."
 	dc.b	$FC
-	dc.b	"I can't believe it."
-	dc.b	$FD
-	dc.b	"That old man!"
+	dc.b	"Ce vieux bizarre!?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"But, he was tricked by those"
+	dc.b	"Mais ya des gens qui étaient"
 	dc.b	$FC
-	dc.b	"who were jealous of him, and"
+	dc.b	"jaloux qui lui ont passé un"
 	dc.b	$FD
-	dc.b	"was banished into a rural place"
+	dc.b	"sapin, pis ya été banni au"
 	dc.b	$FC
-	dc.b	"like this."
+	dc.b	"boutte du monde."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"Really..."
+	dc.b	"Vraiment..."
 	dc.b	$FC
-	dc.b	"it just doesn't possible."
+	dc.b	"Ça ne m'apparait pas plausible."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$15
-	dc.b	"But actually it turned out"
+	dc.b	"Comme de faite, Raja est ben"
 	dc.b	$FC
-	dc.b	"better for him this way!"
+	dc.b	"content de pouvoir se débarasser"
 	dc.b	$FD
-	dc.b	"He's a real good guy!"
+	dc.b	"de ce temple ben trop guindé!"
+	dc.b	$FD
+	dc.b	"C't'un bon gars! Yé vraiment"
 	dc.b	$FC
-	dc.b	"He's a good friend of mine!"
+	dc.b	"un bon ami!"
 	dc.b	$FF
 
 ; $3C
-	dc.b	"Do you want to know about the"
+	dc.b	"Voulez savoir ce qui s'dit à"
 	dc.b	$FC
-	dc.b	"whereabouts of the space ship?"
+	dc.b	"propos du vaisseau spatial?"
 	dc.b	$F5
 	dc.b	$00, $02
 	dc.b	$FA
 	dc.b	$82, $01
-	dc.b	"Are you talking about the space"
+	dc.b	"Ah, vous voulez dire le vaisseau"
 	dc.b	$FC
-	dc.b	"ship that is underground in the"
+	dc.b	"qui est sous terre dans la ville"
 	dc.b	$FD
-	dc.b	"town of Tyler?"
+	dc.b	"de Tyler?"
 	dc.b	$FD
-	dc.b	"I don't know a lot about it, but"
+	dc.b	"J'sais pas grand chose,"
 	dc.b	$FC
-	dc.b	"apparently, if you take a good"
+	dc.b	"remarquez, mais ça a l'air que"
 	dc.b	$FD
-	dc.b	"look at the grave, a path will"
+	dc.b	"si vous r'gardez le tombeau"
 	dc.b	$FC
-	dc.b	"open for you."
+	dc.b	"attentivement, ça va s'ouvrir."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"A grave...?"
+	dc.b	"Le tombeau...?"
 	dc.b	$FF
 
 ; $3D
-	dc.b	"...What's the use now?"
+	dc.b	"...À quoi bon?"
 	dc.b	$FF
 
 ; $3E
-	dc.b	"Thank you."
+	dc.b	"16-3E Merci bien."
 	dc.b	$FF
