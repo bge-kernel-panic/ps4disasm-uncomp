084364b7bbffb01f25866e37c8b97e98bf92bfa5
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2018-03-17 23:00:57 -0400

    WIP: more translations

--- script/dialogue 31.asm
+++ script/dialogue 31.asm
@@ -1,153 +1,155 @@
 ; 0
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" I wonder where Alys is?"
+	dc.b	" Je me demande où est Alys?"
 	dc.b	$FF
 
 ; $1
 	dc.b	$F4
 	dc.b	$02
-	dc.b	" We better see the principal"
+	dc.b	" Il vaudrait mieux aller voir"
 	dc.b	$FC
-	dc.b	"and get more details!"
+	dc.b	"le directeur pour les détails!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"ASo he's the one who requested"
+	dc.b	"AAlors c'est lui qui nous a"
 	dc.b	$FC
-	dc.b	"the commission."
+	dc.b	"donnée le mandat."
 	dc.b	$FD
-	dc.b	"I wonder where he is?"
+	dc.b	"Je me demande où il se trouve?"
 	dc.b	$FF
 
 ; $2
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"AAre you saying that there are"
+	dc.b	"AAlors il y aurait des monstres"
 	dc.b	$FC
-	dc.b	"monsters in the basement of this"
-	dc.b	$FD
-	dc.b	"academy?"
+	dc.b	"dans le sous-sol de l'académie?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
 	dc.b	" Hmm..."
 	dc.b	$FD
-	dc.b	"Sounds like there's something"
+	dc.b	"Il y a quelque chose qui cloche"
 	dc.b	$FC
-	dc.b	"behind all this!"
+	dc.b	"dans tout ça!"
 	dc.b	$FF
 
 ; $3
 	dc.b	$F4
 	dc.b	$02
-	dc.b	" Both the principal and Hahn"
+	dc.b	" Le directeur et Hahn nous ont"
 	dc.b	$FC
-	dc.b	"have asked us to inspect"
+	dc.b	"tout deux demandé d'explorer"
 	dc.b	$FD
-	dc.b	"the basement."
+	dc.b	"le sous-sol."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BAre you sure you're not just"
+	dc.b	"BVous n'avez pas simplement"
 	dc.b	$FC
-	dc.b	"trying to take my money?"
+	dc.b	"trouvé un stratagème pour me"
+	dc.b	$FD
+	dc.b	"détrousser?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"AHey, you've already paid, so"
+	dc.b	"ADésolé, vous avez déjà payé, ce"
 	dc.b	$FC
-	dc.b	"don't cry over spilt milk."
+	dc.b	"qui est fait est fait."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BI wonder if I'll be OK"
+	dc.b	"BJe me demande si c'était une"
 	dc.b	$FC
-	dc.b	"going with these people..."
+	dc.b	"bonne idée de les accompagner..."
 	dc.b	$FF
 
 ; $4
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" I wonder what that capsules"
+	dc.b	" Je me demande à quoi servent"
 	dc.b	$FC
-	dc.b	"in the basement are for?"
+	dc.b	"ces tubes dans le sous-sol?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
-	dc.b	"AI'm going to make the principal"
+	dc.b	"AJe vais forcer le directeur"
 	dc.b	$FC
-	dc.b	"tell us!"
+	dc.b	"à nous le dire!"
 	dc.b	$FD
-	dc.b	"Hey, you!"
+	dc.b	"Hé, toi!"
 	dc.b	$FC
-	dc.b	"You're coming along too!"
+	dc.b	"Tu viens aussi avec nous!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BWho m...me?"
+	dc.b	"BQui, me...moi?"
 	dc.b	$FF
 
 ; $5
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"ABirth Valley is located"
+	dc.b	"ALa Vallée de l'Éclosion est"
 	dc.b	$FC
-	dc.b	"northeast of Piata."
+	dc.b	"nord-est de Piata."
 	dc.b	$FD
-	dc.b	"Let's hurry and go to the"
+	dc.b	"Dépêchons-nous de secourir le"
 	dc.b	$FC
-	dc.b	"professor's rescue!"
+	dc.b	"professeur!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
-	dc.b	" Ah...Zio."
+	dc.b	" Ah...Zio...."
 	dc.b	$FC
-	dc.b	"I don't like the sound of that!"
+	dc.b	"Je n'aime pas ça!"
 	dc.b	$FF
 
 ; $6
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BOh, no! Professor Holt has been"
+	dc.b	"BOh, non! Le professeur Holt a"
 	dc.b	$FC
-	dc.b	"turned to stone!"
+	dc.b	"été pétrifié!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" He'll be all right, Hahn."
+	dc.b	" Ça ira, Hahn. Nous pouvons"
 	dc.b	$FD
-	dc.b	"We can go to Molcum and obtain"
+	dc.b	"obtenir le médicament d'Alshline"
 	dc.b	$FC
-	dc.b	"the medecine Alshline."
+	dc.b	"à Molcum."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BMolcum...you mean that Motavian"
+	dc.b	"BMolcum...vous voulez parler de"
 	dc.b	$FC
-	dc.b	"village south of Zema?"
+	dc.b	"ce village Motavien au sude de"
+	dc.b	$FD
+	dc.b	"Zema?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
-	dc.b	"AOh, calm down!"
+	dc.b	"AAllez, du calme!"
 	dc.b	$FC
-	dc.b	"Really, you're pathetic!"
+	dc.b	"Vraiment, tu es pitoyable!"
 	dc.b	$FF
 
 ; $7
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" Molcum has been"
+	dc.b	" Molcum a été détruite par"
 	dc.b	$FC
-	dc.b	"done in by Zio..."
+	dc.b	"Zio..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"ABy the time I arrived,"
+	dc.b	"AQuand j'y suis arrivé, il était"
 	dc.b	$FC
-	dc.b	"it was too late."
+	dc.b	"déjà trop tard."
 	dc.b	$FD
-	dc.b	"Sorry."
+	dc.b	"Désolé..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
@@ -155,55 +157,55 @@
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BBut, once we get to Tonoe,"
+	dc.b	"BCeci dit, une fois à Tonoe,"
 	dc.b	$FC
-	dc.b	"we'll be able to obtain"
+	dc.b	"nous pourrons obtenir de"
 	dc.b	$FD
-	dc.b	"Alshline, right?"
+	dc.b	"l'Alshline, n'est-ce pas?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" Tonoe...now that's north"
+	dc.b	" Tonoe...c'est au nord du"
 	dc.b	$FC
-	dc.b	"of the village of Krup, right?"
+	dc.b	"village de Krup, je crois?"
 	dc.b	$FF
 
 ; $8
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" So there's Alshline in the"
+	dc.b	" Alors l'Alshline se trouve dans"
 	dc.b	$FC
-	dc.b	"underground warehouse of Tonoe?"
+	dc.b	"l'entrepôt souterrain de Tonoe?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
-	dc.b	"AThanks for"
-	dc.b	$FC
-	dc.b	"being our guide, Gryz."
+	dc.b	"AMerci d'être notre guide, Gryz."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$05
-	dc.b	"BNo problem."
+	dc.b	"BAvec plaisir. Vous pouvez"
 	dc.b	$FC
-	dc.b	"You can leave everything to me."
+	dc.b	"compter sur moi."
 	dc.b	$FF
 
 ; $9
 	dc.b	$F4
 	dc.b	$02
-	dc.b	" We've finally obtained"
+	dc.b	" Finalement, nous avons de"
 	dc.b	$FC
-	dc.b	"the Alshline!"
+	dc.b	"l'Alshline!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"ALet's hurry to Zema and rescue"
+	dc.b	"ADépêchons-nous de nous rendre"
 	dc.b	$FC
-	dc.b	"the professor and his team."
+	dc.b	"à Zema et sauver le professeur"
+	dc.b	$FD
+	dc.b	"et son équipe."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$05
-	dc.b	"BI'll join you."
+	dc.b	"BJe viens avec vous."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
@@ -213,195 +215,195 @@
 ; $A
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"AProfessor Holt has gone into"
+	dc.b	"ALe professeur Holt a pénétré"
 	dc.b	$FC
-	dc.b	"the depths of Birth Valley and"
+	dc.b	"dans les profondeurs de la"
 	dc.b	$FD
-	dc.b	"hasn't come back since."
+	dc.b	"Vallée de l'Éclosion et aucun"
+	dc.b	$FC
+	dc.b	"signe de lui depuis."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"BAfter all we did to obtain"
+	dc.b	"BAprès tout ce travail pour"
 	dc.b	$FC
-	dc.b	"the Alshline to turn him from"
+	dc.b	"trouver de l'Alshline et"
 	dc.b	$FD
-	dc.b	"stone back to flesh."
+	dc.b	"renverser sa pétréfication..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$02
-	dc.b	" This teacher of yours is such a"
+	dc.b	" Ton professeur est vraiment"
 	dc.b	$FC
-	dc.b	"pain in the butt!"
+	dc.b	"un vrai casse-pied!"
 	dc.b	$FF
 
 ; $B
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"AWe've got to hurry and stop"
+	dc.b	"ANous devons nous dépêcher de"
 	dc.b	$FC
-	dc.b	"the system that's running out of"
+	dc.b	"mettre hors service ce système"
 	dc.b	$FD
-	dc.b	"control...for Seed's sake, too!"
+	dc.b	"hors de contrôle...pour honorer"
+	dc.b	$FC
+	dc.b	"la mémoire de Seed, également!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" First, we'll have to rescue"
+	dc.b	" D'abord, nous devons libérer"
 	dc.b	$FC
-	dc.b	"this android called Demi."
+	dc.b	"cet androïde appelé Demi."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$05
-	dc.b	"BWe'll have to go to Zio's Fort."
+	dc.b	"BAlors il faut aller au fort de"
 	dc.b	$FC
-	dc.b	"Just what we all wanted to do!"
+	dc.b	"Zio. C'est ce qu'on voulait"
+	dc.b	$FD
+	dc.b	"de toute façon!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$03
-	dc.b	"AYou're talking about that castle"
+	dc.b	"AVous voulez parler de ce château"
 	dc.b	$FC
-	dc.b	"beyond the quicksand that can be"
+	dc.b	"passé les sables mouvants qu'on"
 	dc.b	$FD
-	dc.b	"seen from the village of Mile."
+	dc.b	"distingue du village de Mile?"
 	dc.b	$FD
-	dc.b	"I guess we'll have to go around"
+	dc.b	"Il faudra donc faire le tour par"
 	dc.b	$FC
-	dc.b	"from the north."
+	dc.b	"le nord."
 	dc.b	$FF
 
 ; $C
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" Oh no!"
+	dc.b	" Oh non!"
 	dc.b	$FC
-	dc.b	"Alys is..."
+	dc.b	"Alys a été..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"AChaz, let's do as Alys says"
+	dc.b	"AChaz, suivons les conseils"
 	dc.b	$FC
-	dc.b	"and find this person called"
-	dc.b	$FD
-	dc.b	"Rune."
+	dc.b	"d'Alys et trouvons ce Rune."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$07
-	dc.b	"BWe'll need the 'Land Rover'"
+	dc.b	"BNous aurons besoin du 'Land"
 	dc.b	$FC
-	dc.b	"to cross the quicksand."
+	dc.b	"Rover' pour passer les sables"
 	dc.b	$FD
-	dc.b	"Let's find the 'Machine Center.'"
+	dc.b	"mouvants. Dirigeons-nous vers"
 	dc.b	$FC
-	dc.b	"It's located south of"
+	dc.b	"le Centre des Machines. Il se"
 	dc.b	$FD
-	dc.b	" the village of Krup!"
+	dc.b	"trouve au sud de Krup!"
 	dc.b	$FF
 
 ; $D
 	dc.b	$F4
 	dc.b	$07
-	dc.b	" The Land Rover is"
+	dc.b	" Le Land Rover est à l'intérieur"
 	dc.b	$FC
-	dc.b	"inside the Machine Center!"
+	dc.b	"du Centre des Machines!"
 	dc.b	$FD
-	dc.b	"Let's hurry!"
+	dc.b	"Faisons vite!"
 	dc.b	$FF
 
 ; $E
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" OK! "
-	dc.b	$FC
-	dc.b	"We've got the Land Rover!"
+	dc.b	" OK! Nous avons le Land Rover!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$05
-	dc.b	"BNow we can cross the quicksand"
+	dc.b	"BAlors nous pourrons traverser"
 	dc.b	$FC
-	dc.b	"to the east and get to"
+	dc.b	"les sables mouvants à l'est et"
 	dc.b	$FD
-	dc.b	"The Ladea Tower."
+	dc.b	"entrer dans la Tour Ladea."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"AAnd there's supposed to be"
+	dc.b	"AEt ce Rune est censé s'y"
 	dc.b	$FC
-	dc.b	"a person called Rune there."
+	dc.b	"trouver."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" I don't feel much like seeing"
+	dc.b	" Le revoir ne me fera guère"
 	dc.b	$FC
-	dc.b	"him,"
+	dc.b	"plaisir, mais..."
 	dc.b	$FD
-	dc.b	"but I guess we have no choice!"
+	dc.b	"j'imagine qu'on a pas le choix!"
 	dc.b	$FF
 
 ; $F
 	dc.b	$F4
 	dc.b	$04
-	dc.b	" To break through Zio's magic"
+	dc.b	" Pour neutraliser la barrière"
 	dc.b	$FC
-	dc.b	"barrier,"
+	dc.b	"magique de Zio, nous devons"
 	dc.b	$FD
-	dc.b	"we'll need the 'Psycho-wand.'"
+	dc.b	"obtenir le 'Bâton-psy.'"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BAnd it's on the topmost floor"
+	dc.b	"BEt il se trouve au sommet de"
 	dc.b	$FC
-	dc.b	"of The Ladea Tower?"
+	dc.b	"la Tour Ladea?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"ALet's defeat Zio"
-	dc.b	$FC
-	dc.b	"with that wand!"
+	dc.b	"ADétruisons Zio avec ce bâton!"
 	dc.b	$FD
-	dc.b	"And...Alys needs to"
+	dc.b	"Et...Alys doit se rétablir"
 	dc.b	$FC
-	dc.b	"get better quick!"
+	dc.b	"rapidement!"
 	dc.b	$FF
 
 ; $10
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" We've defeated Zio's underlings,"
+	dc.b	" Nous avons vaincus les"
 	dc.b	$FC
-	dc.b	"but..."
+	dc.b	"subalternes de Zio..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"AIt seems that the Psycho-wand"
+	dc.b	"AIl semblerait que le Bâton-psy"
 	dc.b	$FC
-	dc.b	"works well against him."
+	dc.b	"est efficace contre lui."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$05
-	dc.b	"BLet's get it quickly before"
+	dc.b	"BTrouvons-le rapidement avant"
 	dc.b	$FC
-	dc.b	"the next ones come."
+	dc.b	"que d'autres ennemis approchent."
 	dc.b	$FF
 
 ; $11
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"AAs long as we have"
+	dc.b	"ATant que le Bâton-psy est en"
 	dc.b	$FC
-	dc.b	"the Psycho-wand,"
+	dc.b	"notre possession, nous n'avons"
 	dc.b	$FD
-	dc.b	"we needn't be afraid of"
+	dc.b	"pas à craindre les pouvoirs de"
 	dc.b	$FC
-	dc.b	"Zio's power."
+	dc.b	"Zio."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$07
-	dc.b	"BLet's go to Nurvus in"
+	dc.b	"BDirigeons-nous vers Nurvus dans"
 	dc.b	$FC
-	dc.b	"the basement of Zio's Fort...!"
+	dc.b	"le sous-sol du Fort de Zio...!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" Blast! Alys...!"
+	dc.b	" Tonnerre! Alys...!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
@@ -411,27 +413,27 @@
 ; $12
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"AWe've broken the invisible"
+	dc.b	"ANous avons détruit la barrière"
 	dc.b	$FC
-	dc.b	"barrier."
+	dc.b	"invisible."
 	dc.b	$FD
-	dc.b	"Now all we have to do is get to"
+	dc.b	"Tout ce qui nous reste à faire,"
 	dc.b	$FC
-	dc.b	"Nurvus!"
+	dc.b	"c'est d'entrer dans Nurvus!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$07
-	dc.b	"BWe can't abandon the situation"
+	dc.b	"BNous ne pouvons ignorer la"
 	dc.b	$FC
-	dc.b	"with Zio, but yes,"
+	dc.b	"situation par rapport à Zio,"
 	dc.b	$FD
-	dc.b	"stopping Nurvus is a higher"
+	dc.b	"mais effectivement, neutraliser"
 	dc.b	$FC
-	dc.b	"priority!"
+	dc.b	"Nurvus est prioritaire!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" Yes, that's true..."
+	dc.b	" Ouais, c'est vrai..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
@@ -441,77 +443,77 @@
 ; $13
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" We've been able to stop Nurvus,"
+	dc.b	" Nurvus est neutralisé grâce à"
 	dc.b	$FC
-	dc.b	"thanks to Demi, but..."
+	dc.b	"l'aide de Demi, mais..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"AWe've got to go to the"
+	dc.b	"ANous devons atteindre le"
 	dc.b	$FC
-	dc.b	"artificial satellite 'Zelan' to"
+	dc.b	"satellite artificiel Zelan pour"
 	dc.b	$FD
-	dc.b	"restore the functions of the"
+	dc.b	"rétablir le système de contrôle"
 	dc.b	$FC
-	dc.b	"environmental control system."
+	dc.b	"environnemental."
 	dc.b	$FD
-	dc.b	"And Wren is there, right?"
+	dc.b	"Et Wren s'y trouve, pas vrai?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"AWhatever."
+	dc.b	"AOuais, ouais."
 	dc.b	$FC
-	dc.b	"Let's just hurry to 'Zelan!'"
+	dc.b	"Ne tardons pas, cap sur Zelan!"
 	dc.b	$FF
 
 ; $14
 	dc.b	$F4
 	dc.b	$08
-	dc.b	" The cause of the system's"
+	dc.b	" La raison des anomalies du"
 	dc.b	$FC
-	dc.b	"abnormalities turns out to be"
+	dc.b	"système se trouve venir de"
 	dc.b	$FD
-	dc.b	"in 'Kuran.'"
+	dc.b	"Kuran."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BThe artificial satellite 'Kuran'"
+	dc.b	"BLe satellite artificiel Kuran..."
 	dc.b	$FC
-	dc.b	"...what did you say was"
+	dc.b	"...que disiez-vous se passe-t-il"
 	dc.b	$FD
-	dc.b	"happening there?"
+	dc.b	"là-bas?"
 	dc.b	$FF
 
 ; $15
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" To get to Kuran,"
+	dc.b	" Pour aller à Kuran, il faut"
 	dc.b	$FD
-	dc.b	"we'll have to find that"
+	dc.b	"trouver ce vaisseau spatial dont"
 	dc.b	$FC
-	dc.b	"space ship Raja told us about."
+	dc.b	"Raja nous a parlé."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BSo that means we're going to"
+	dc.b	"BDonc, il faut nous diriger vers"
 	dc.b	$FC
-	dc.b	"the town of Tyler northwest of"
+	dc.b	"la ville de Tyler, au nord-ouest"
 	dc.b	$FD
-	dc.b	"Raja's temple?"
+	dc.b	"du temple de Raja?"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$09
-	dc.b	"ABefore doing that,"
+	dc.b	"ATout d'abord, pourquoi ne pas"
 	dc.b	$FC
-	dc.b	"why don't we take a little break"
+	dc.b	"se reposer un peu à l'auberge"
 	dc.b	$FD
-	dc.b	"at the inn in Ryuon?"
+	dc.b	"à Ryuon?"
 	dc.b	$FD
-	dc.b	"You'll be able to hear a lot of"
+	dc.b	"Vous pourrez y entendre plein"
 	dc.b	$FC
-	dc.b	"fun stories,"
+	dc.b	"d'histoires amusantes, ha ha!"
 	dc.b	$FD
-	dc.b	"you won't regret it, ha, ha!"
+	dc.b	"Vous ne le regretterez pas!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
@@ -521,23 +523,25 @@
 ; $16
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" According to Gyuna's story,"
+	dc.b	" D'après ce que Gyuna a dit,"
 	dc.b	$FD
-	dc.b	"the space ship is underground"
+	dc.b	"le vaisseau spatial se trouve"
 	dc.b	$FC
-	dc.b	"in the town of Tyler."
+	dc.b	"sous terre à Tyler."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BI hear there's some secret"
+	dc.b	"BJ'ai entendu dire qu'il y avait"
 	dc.b	$FC
-	dc.b	"with the grave..."
+	dc.b	"un secret par rapport à ce"
+	dc.b	$FD
+	dc.b	"tombeau..."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$09
-	dc.b	"AThis seems like a planned"
+	dc.b	"AOn dirait un hasard heureux."
 	dc.b	$FC
-	dc.b	"turn of events. Ha, ha!"
+	dc.b	"Ha, ha, ha!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
@@ -547,83 +551,83 @@
 ; $17
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"AWell, we've found the 'Landale'"
+	dc.b	"ABon, nous avons trouvé le"
 	dc.b	$FD
-	dc.b	"so we'll just have to go to"
+	dc.b	"'Landale', alors cap sur"
 	dc.b	$FC
 	dc.b	"Kuran."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BFinally!"
+	dc.b	"BEnfin!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$08
-	dc.b	"ALet's immediately identify"
+	dc.b	"ADépèchons-nous d'identifier la"
 	dc.b	$FC
-	dc.b	"reason the environmental control"
+	dc.b	"cause de la perte de contrôle"
 	dc.b	$FD
-	dc.b	"system is out of control."
+	dc.b	"du système environnemental."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" OK. Let's go!"
+	dc.b	" D'accord. Allons-y!"
 	dc.b	$FF
 
 ; $18
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BWe've beaten Dark Force,"
+	dc.b	"BNous avons vaincu Dark Force,"
 	dc.b	$FC
-	dc.b	"but Dezolis is not recovering!"
+	dc.b	"mais Dezolis ne s'en remet pas!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$09
-	dc.b	"AI've been telling you,"
+	dc.b	"AJe me tues à vous le dire,"
 	dc.b	$FD
-	dc.b	"the problem lies in"
+	dc.b	"le problème vient de la Tour"
 	dc.b	$FC
-	dc.b	"the Garuberk Tower!"
+	dc.b	"Garuberk!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$08
-	dc.b	"AIn any event, I do acknowledge"
+	dc.b	"AEn tout état de cause, je dois"
 	dc.b	$FC
-	dc.b	"there's a need to investigate"
+	dc.b	"admettre la nécessité d'enquêter"
 	dc.b	$FD
-	dc.b	"Dezolis."
+	dc.b	"sur Dezolis."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$06
-	dc.b	"BWell, what are we waiting for?"
+	dc.b	"BBon, qu'est-ce qu'on attend?"
 	dc.b	$FC
-	dc.b	"Let's go!"
+	dc.b	"Allons-y!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	" Hmm...the Garuberk Tower..."
+	dc.b	" Hmm...la Tour Garuberk..."
 	dc.b	$FF
 
 ; $19
 	dc.b	$F4
 	dc.b	$06
-	dc.b	" Apparently Kyra from Esper has"
+	dc.b	" Alors cette Esper Kyra s'est"
 	dc.b	$FC
-	dc.b	"headed towards the Garuberk"
+	dc.b	"dirigée vers la Tour Garuberk"
 	dc.b	$FD
-	dc.b	"Tower alone!"
+	dc.b	"toute seule!"
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$04
-	dc.b	"BI've heard the forest"
+	dc.b	"BJ'ai entendu dire que la forêt"
 	dc.b	$FC
-	dc.b	"surrounding the tower is"
+	dc.b	"qui encercle la Tour est emplie"
 	dc.b	$FD
-	dc.b	"full of carnivorous trees."
+	dc.b	"d'arbres carnivores."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
-	dc.b	"AWe can't let her go alone."
+	dc.b	"ANous ne pouvons pas la laisser"
 	dc.b	$FC
-	dc.b	"We've got to go help her!"
+	dc.b	"à elle-même. Allons l'aider!"
 	dc.b	$FF
