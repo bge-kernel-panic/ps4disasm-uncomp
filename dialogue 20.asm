2735499f568d5dfcaadaa5c1d51c74c64072bf81
galletteuh <46330714+galletteuh@users.noreply.github.com>
2019-01-08 08:35:03 +0100

    Corrections
    Corrections après un run en ligne droite du jeu

---
+++ dialogue 20.asm
@@ -0,0 +1,853 @@
+; 0
+	dc.b	$FF
+
+; $1
+	dc.b	$FF
+
+; $2
+	dc.b	$FF
+
+; $3
+	dc.b	$FF
+
+; $4
+	dc.b	$FF
+
+; $5
+	dc.b	$FF
+
+; $6
+	dc.b	$FF
+
+; $7
+	dc.b	$FF
+
+; $8
+	dc.b	$FF
+
+; $9
+	dc.b	$FF
+
+; $A
+	dc.b	$FF
+
+; $B
+	dc.b	$FF
+
+; $C
+	dc.b	$FF
+
+; $D
+	dc.b	$FF
+
+; $E
+	dc.b	$FF
+
+; $F
+	dc.b	$FF
+
+; $10
+	dc.b	$FF
+
+; $11
+	dc.b	$FF
+
+; $12
+	dc.b	$FF
+
+; $13
+	dc.b	$FF
+
+; $14
+	dc.b	$FF
+
+; $15
+	dc.b	$FF
+
+; $16
+	dc.b	$FF
+
+; $17
+	dc.b	$FF
+
+; $18
+	dc.b	$FF
+
+; $19
+	dc.b	$FF
+
+; $1A
+	dc.b	$FF
+
+; $1B
+	dc.b	$FF
+
+; $1C
+	dc.b	$FF
+
+; $1D
+	dc.b	$FF
+
+; $1E
+	dc.b	$FF
+
+; $1F
+	dc.b	$FF
+
+; $20
+	dc.b	$FF
+
+; $21
+	dc.b	$FF
+
+; $22
+	dc.b	$FF
+
+; $23
+	dc.b	$F4
+	dc.b	$17
+	dc.b	"Excusez moi!"
+	dc.b	$FF
+
+; $24
+	dc.b	$FA
+	dc.b	$A2, $01
+	dc.b	$F6
+	dc.w	$0051	; => Event_InnerSanctGuard
+	dc.b	$FF
+
+; $25
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Tout est conforme aux souhaits"
+	dc.b	$FC
+	dc.b	"du vénéré Cinquième."
+	dc.b	$FF
+
+; $26
+	dc.b	$FA
+	dc.b	$D9, $03
+	dc.b	$FA
+	dc.b	$A3, $02
+	dc.b	$FA
+	dc.b	$D7, $01
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Je prie pour votre sécurité."
+	dc.b	$FF
+
+; $27
+	dc.b	$F6
+	dc.w	$0052	; => Event_InnerSanctGuardBeforeElsydeon
+	dc.b	$FF
+
+; $28
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Je crois en vous, vénéré"
+	dc.b	$FC
+	dc.b	"Cinquième."
+	dc.b	$FF
+
+; $29
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Quelle surprise, vous avez"
+	dc.b	$FC
+	dc.b	"réussi à obtenir cet objet..."
+	dc.b	$FD
+	dc.b	"Comptez sur le vénéré Cinquième"
+	dc.b	$FC
+	dc.b	"pour pouvoir choisir celui qui"
+	dc.b	$FD
+	dc.b	"les bonnes aptitudes!"
+	dc.b	$FF
+
+; $2A
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Ici, c'est le sanctuaire"
+	dc.b	$FC
+	dc.b	"intérieur, et plus loin..."
+	dc.b	$FD
+	dc.b	"Il y a une chambre applée la"
+	dc.b	$FC
+	dc.b	"Chambre de Lutz."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Exact. Alors, le temps est venu!"
+	dc.b	$FD
+	dc.b	"Enfin, nous pourrons rencontrer"
+	dc.b	$FC
+	dc.b	"Lutz!"
+	dc.b	$FD
+	dc.b	"Oh là là! Mes vêtements sont"
+	dc.b	$FC
+	dc.b	"propres? Mes cheveux coiffés?"
+	dc.b	$FD
+	dc.b	"Oh, que faire! Je suis si"
+	dc.b	$FC
+	dc.b	"nerveuse!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Je me demande quel genre de"
+	dc.b	$FC
+	dc.b	"personne est-il? Qu'en"
+	dc.b	$FD
+	dc.b	"penses-tu, Chaz?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Hum, Lutz.. "
+	dc.b	$FC
+	dc.b	"Le magicien légendaire..."
+	dc.b	$FF
+
+; $2B
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Là, laissez-moi vous guérir."
+	dc.b	$FC
+	dc.b	"Ensuite, allez-y!"
+	dc.b	$FF
+
+; $2C
+	dc.b	$FF
+
+; $2D
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Chaz...ressaisis-toi!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$08
+	dc.b	"Les êtres humains sont bien"
+	dc.b	$FC
+	dc.b	"difficiles à comprendre."
+	dc.b	$FF
+
+; $2E
+	dc.b	$F4
+	dc.b	$17
+	dc.b	"Kyra! Vous ne pouvez pas passer"
+	dc.b	$FC
+	dc.b	"sans autorisation!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Depuis quand je dois avoir une"
+	dc.b	$FC
+	dc.b	"autorisation!?"
+	dc.b	$FD
+	dc.b	"Ouvrez-nous la porte!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$17
+	dc.b	"Cela m'est impossible!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Espèce de gros balourd têtu!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Laissez-la entrer!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$17
+	dc.b	"Comment osez-vous! Je ne peux..."
+	dc.b	$FD
+	dc.b	"Hé!"
+	dc.b	$FD
+	dc.b	"Ah, c'est... c'est vous!"
+	dc.b	$FD
+	dc.b	"Excusez mon impolitesse!"
+	dc.b	$FC
+	dc.b	"Entrez, je vous prie!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Rune...?"
+	dc.b	$FF
+
+; $2F
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Euh...que se passe-t-il?"
+	dc.b	$FC
+	dc.b	"Il n'y a personne ici..."
+	dc.b	$FD
+	dc.b	"Vénéré Lutz!"
+	dc.b	$FC
+	dc.b	"Veuillez vous montrer!"
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Lutz n'est pas ici... Il n'est"
+	dc.b	$FC
+	dc.b	"plus de ce monde depuis"
+	dc.b	$FD
+	dc.b	"longtemps!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Comment?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$08
+	dc.b	$F2, $00
+	dc.w	$009F
+	dc.b	"Aucun humain ne pourrait vivre"
+	dc.b	$FC
+	dc.b	"plus de deux mille ans."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"C'est impossible! Je l'ai vu"
+	dc.b	$FC
+	dc.b	"de mes propres yeux!"
+	dc.b	$FD
+	dc.b	"De plus...de plus... Nous avons"
+	dc.b	$FC
+	dc.b	"tous la foi, pas vrai?"
+	dc.b	$FD
+	dc.b	"Nous croyons dans la légende des"
+	dc.b	$FC
+	dc.b	"Espers, qui dit que Lutz est"
+	dc.b	$FD
+	dc.b	"toujours vivant et nous guide,"
+	dc.b	$FC
+	dc.b	"n'est-ce pas!?"
+	dc.b	$FD
+	dc.b	"C'est pourquoi...pourquoi..."
+	dc.b	$FC
+	dc.b	"nous travaillons si dur..."
+	dc.b	$FD
+	dc.b	"il doit encore exister!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Kyra! Calme-toi... c'est vrai"
+	dc.b	$FC
+	dc.b	"que Lutz n'est plus de ce monde."
+	dc.b	$FD
+	dc.b	"Mais même si son corps n'est"
+	dc.b	$FC
+	dc.b	"plus, son esprit vit toujours."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Comment?"
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0100
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Avant de mourir, il a stocké sa"
+	dc.b	$FC
+	dc.b	"volonté et sa mémoire dans la"
+	dc.b	$FD
+	dc.b	"Boule Télépathique."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Donc...vous dites que cette"
+	dc.b	$FC
+	dc.b	"Boule est, en fait, Lutz?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Non. Il n'est plus là. Mais si"
+	dc.b	$FC
+	dc.b	"la personne choisie s'approche,"
+	dc.b	$FD
+	dc.b	"la volonté et la mémoire de Lutz"
+	dc.b	$FC
+	dc.b	"lui seront transmises."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Personne choisie...?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Donc..."
+	dc.b	$F2, $02
+	dc.b	$F9
+	dc.b	$B3
+
+	dc.b	$F2, $00
+	dc.w	$0101
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Attendez!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Rune...est Lutz?!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"C'est exact. Rune Walsh..."
+	dc.b	$FC
+	dc.b	"Est le Cinquième Lutz."
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0102
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Vous...vous êtes..."
+	dc.b	$FC
+	dc.b	"Lutz...? Comment est-ce..."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Écoute bien, Chaz."
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0103
+	dc.b	"Depuis les temps immémoriaux"
+	dc.b	$FC
+	dc.b	"dans le système Algo, il y a un"
+	dc.b	$FD
+	dc.b	"cycle de calamités qui se répète"
+	dc.b	$FC
+	dc.b	"tous les mille ans."
+	dc.b	$FD
+	dc.b	"La cause est l'incarnation du"
+	dc.b	$FC
+	dc.b	"mal appelée Dark Force, qui"
+	dc.b	$FD
+	dc.b	"ressuscite tous les mille ans!"
+	dc.b	$FD
+	dc.b	"Jusqu'ici, durant chaque cycle,"
+	dc.b	$FC
+	dc.b	"un individu courageux vainquit"
+	dc.b	$FD
+	dc.b	"le mal pour préserver la paix."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Mais nous avons vaincu Dark"
+	dc.b	$FC
+	dc.b	"Force sur Kuran, c'est sûr!"
+	dc.b	$FD
+	dc.b	"Pourtant, il n'y a pas"
+	dc.b	$FC
+	dc.b	"d'amélioration des conditions"
+	dc.b	$FD
+	dc.b	"climatiques à travers Algo!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Alors, que se passe-t-il?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Algo a été destabilisé il y a"
+	dc.b	$FC
+	dc.b	"mille ans par l'explosion de"
+	dc.b	$FD
+	dc.b	"Parma."
+	dc.b	$FD
+	dc.b	"L'équilibre précaire qui avait"
+	dc.b	$FC
+	dc.b	"été maintenu jusqu'alors fut"
+	dc.b	$FD
+	dc.b	"bouleversé."
+	dc.b	$FD
+	dc.b	"C'est probablement la raison"
+	dc.b	$FC
+	dc.b	"pour laquelle nous pouvons"
+	dc.b	$FD
+	dc.b	"toujours percevoir l'Onde"
+	dc.b	$FC
+	dc.b	"d'Énergie Noire."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Tu veux dire que Dark Force a"
+	dc.b	$FC
+	dc.b	"survécu?!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Je... je n'en suis pas sûr."
+	dc.b	$FD
+	dc.b	$F2, $02
+	dc.b	$F2, $00
+	dc.w	$0104
+	dc.b	"Mais je peux vous affirmer que"
+	dc.b	$FC
+	dc.b	"la racine du mal existe encore!"
+	dc.b	$FD
+	dc.b	"La seule chose à faire est de la"
+	dc.b	$FC
+	dc.b	"détruire!"
+	dc.b	$FD
+	dc.b	"Je t'ai choisi, Chaz!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Qui, moi?!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Je t'ai choisi. Tu est le soldat"
+	dc.b	$FC
+	dc.b	"qui livrera l'ultime combat!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Pourquoi...pourquoi moi?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Je perçoit ton potentiel."
+	dc.b	$FD
+	dc.b	"Nous saurons bientôt si mon"
+	dc.b	$FC
+	dc.b	"intuition se confirme, Chaz."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Ouais, super..."
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0105
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Voilà, vous savez maintenant"
+	dc.b	$FC
+	dc.b	"l'histoire. Désolé d'avoir gardé"
+	dc.b	$FD
+	dc.b	"tout cela pour moi. Nous pouvons"
+	dc.b	$FC
+	dc.b	"continuer à travailler ensemble?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Euh, oui, oui..."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Bien! Nous devons nous diriger"
+	dc.b	$FC
+	dc.b	"vers la Tour Garuberk!"
+	dc.b	$FD
+	dc.b	"C'est là que nous trouverons"
+	dc.b	$FC
+	dc.b	"la réponse à nos questions!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$19
+	dc.b	$F2, $00
+	dc.w	$0106
+	dc.b	"Des arbres carnivores empêchent"
+	dc.b	$FC
+	dc.b	"le passage."
+	dc.b	$FD
+	dc.b	"Mais si vous utilisez le"
+	dc.b	$FC
+	dc.b	"Flambeau de l'Éclipse, vous"
+	dc.b	$FD
+	dc.b	"pourrez les détruire."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"D'accord, voilà notre prochain"
+	dc.b	$FC
+	dc.b	"objectif! Le Temple Gumbious."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Le temple Dezolisien est situé"
+	dc.b	$FC
+	dc.b	"dans la vallée montagneuse à"
+	dc.b	$FD
+	dc.b	"l'ouest d'ici."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Allons-y. Allez, Chaz,"
+	dc.b	$FC
+	dc.b	"ressaisis-toi!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Je suis déçue de voir que Lutz"
+	dc.b	$FC
+	dc.b	"est si indélicat!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Navré, j'ai toujours été comme"
+	dc.b	$FC
+	dc.b	"ça!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$0A
+	dc.b	"Ouiiiin."
+	dc.b	$FF
+
+; $30
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Chaz, l'épée sacrée, Elsydeon,"
+	dc.b	$FC
+	dc.b	"t'attends!"
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0128
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Elsydeon?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Il s'agit sans doute de la"
+	dc.b	$FC
+	dc.b	"seule arme capable de détruire"
+	dc.b	$FD
+	dc.b	"la Noirceur Profonde!"
+	dc.b	$FD
+	dc.b	"Va, Chaz...seul! Va et fait"
+	dc.b	$FC
+	dc.b	"face à Elsydeon!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Faire face à Elsydeon?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Exact. Et tu trouveras alors"
+	dc.b	$FC
+	dc.b	"réponse à tes questions."
+	dc.b	$FF
+
+; $31
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Alors voila l'épée sacrée,"
+	dc.b	$FC
+	dc.b	"Elsydeon..."
+	dc.b	$FD
+	dc.b	$F9
+	dc.b	$3B
+
+	dc.b	$F2, $00
+	dc.w	$012A
+	dc.b	"Hein? J'entends une voix!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$00
+	dc.b	"Ah, vous avez réussi,"
+	dc.b	$FC
+	dc.b	"Chaz Ashley..."
+	dc.b	$FD
+	dc.b	"Vous êtes à l'endroit où les"
+	dc.b	$FC
+	dc.b	"esprits de ceux qui se sont"
+	dc.b	$FD
+	dc.b	"battus pour Algo retournent..."
+	dc.b	$FD
+	dc.b	"Elsydeon est l'épée que"
+	dc.b	$FC
+	dc.b	"protègent ces esprits!"
+	dc.b	$FD
+	dc.b	"Maintenant, Chaz, vous devez"
+	dc.b	$FC
+	dc.b	"prendre Elsydeon en vos mains..."
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Hé!"
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Wow!"
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$00
+	dc.b	"Vous êtes donc Chaz, désigné par"
+	dc.b	$FC
+	dc.b	"Rune..."
+	dc.b	$FD
+	dc.b	"Vous êtes en mesure de"
+	dc.b	$FC
+	dc.b	"comprendre."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Aaah...!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$00
+	dc.b	$F2, $00
+	dc.w	$0137
+	dc.b	"Chaz... Je vous confie la charge"
+	dc.b	$FC
+	dc.b	"de porter Elsydeon!"
+	dc.b	$FD
+	dc.b	"Toutes nos pensées sont"
+	dc.b	$FC
+	dc.b	"accumulées dans cette épée."
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0138
+	dc.b	"Nous serons tous derrière vous,"
+	dc.b	$FC
+	dc.b	"Chaz, en tout temps, alors..."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Ne vous en faites pas."
+	dc.b	$FC
+	dc.b	"Je m'en charge!"
+	dc.b	$FF
+
+; $32
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"J'en était sûr. Elsydeon avait"
+	dc.b	$FC
+	dc.b	"promis qu'elle te donnerait"
+	dc.b	$FD
+	dc.b	"son pouvoir."
+	dc.b	$FD
+	dc.b	"Mon jugement est infaillible!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Rune!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	$F2, $00
+	dc.w	$013C
+	dc.b	"Chaz, allons sauver Algo!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Oui! Nous faisons cela pour"
+	dc.b	$FD
+	dc.b	"toute vie dans Algo, et pour"
+	dc.b	$FC
+	dc.b	"forger un avenir réellement"
+	dc.b	$FD
+	dc.b	"délivré du mal!"
+	dc.b	$FD
+	dc.b	"Un avenir finalement libre"
+	dc.b	$FC
+	dc.b	"du mal ancien!"
+	dc.b	$FD
+	dc.b	"Je me batterai pour cela!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Chaz..."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Nous vainquerons, Chaz!"
+	dc.b	$FC
+	dc.b	"Pour Algo!"
+	dc.b	$F9
+	dc.b	$3B
+
+	dc.b	$FD
+	dc.b	$F2, $03
+	dc.b	$A9
+	dc.b	$F4
+	dc.b	$08
+	dc.b	"Il y a une urgence!"
+	dc.b	$FD
+	dc.b	"Demi nous informe d'un désastre"
+	dc.b	$FC
+	dc.b	"qui s'est déclaré sur Motavia!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Chaz!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"C'est bon."
+	dc.b	$FC
+	dc.b	"Pressons, vers Motavia!!"
+	dc.b	$FF
+
+; $33
+	dc.b	$F6
+	dc.w	$004F	; => Event_EsperGuardPermission
+	dc.b	$FF
+
+; $34
+	dc.b	$F6
+	dc.w	$0045	; => Event_PersistentEsperGuards
+	dc.b	$FF
+
+; $35
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Ah, Rune! Content de voir que"
+	dc.b	$FC
+	dc.b	"vous êtes arrivé sain et sauf."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Trêve de formalités."
+	dc.b	$FC
+	dc.b	"Je vais entrer.."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Oh, oui, bien sûr..."
+	dc.b	$FC
+	dc.b	"À vos ordres."
+	dc.b	$FF
+
+; $36
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Nous allons emprunter la chambre"
+	dc.b	$FC
+	dc.b	"intérieur de nouveau. Je dois"
+	dc.b	$FD
+	dc.b	"léguer quelque chose à Chaz."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Euh...Vous n'allez surement pas"
+	dc.b	$FC
+	dc.b	"donner ça à un tel jeunôt!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Jeunôt?!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$04
+	dc.b	"Chaz est le seul choix possible."
+	dc.b	$FC
+	dc.b	"...Vous devez me croire!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$19
+	dc.b	"Entendu, vénéré Cinquième."
+	dc.b	$FC
+	dc.b	"J'obéirai sans protester."
+	dc.b	$FF
+
+; $37
+	dc.b	$F6
+	dc.w	$0067	; => Event_RuneHealingChaz
+	dc.b	$FF
---
+++ dialogue 36.asm
@@ -0,0 +1,301 @@
+; 0
+	dc.b	$F4
+	dc.b	$13
+	dc.b	"C'est...c'est impossible!"
+	dc.b	$FC
+	dc.b	"Je devais être immortel!"
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$006E
+	dc.b	"Pourquoi..."
+	dc.b	$FC
+	dc.b	"Pourquoi m'abandonnes-tu?"
+	dc.b	$FD
+	dc.b	"Dark Fo...Aaaargh!"
+	dc.b	$F2, $03
+	dc.b	$B9
+	dc.b	$F9
+	dc.b	$09
+
+	dc.b	$F2, $03
+	dc.b	$B9
+	dc.b	$F9
+	dc.b	$09
+
+	dc.b	$F2, $03
+	dc.b	$B9
+	dc.b	$F9
+	dc.b	$27
+
+	dc.b	$FD
+	dc.b	$F2, $03
+	dc.b	$9E
+	dc.b	$F2, $00
+	dc.w	$006F
+	dc.b	$F4
+	dc.b	$05
+	dc.b	"Nous avons réussi! Nous avons"
+	dc.b	$FC
+	dc.b	"battu Zio! Youpiiiii!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Alys!"
+	dc.b	$FD
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"À ce stade-ci, impossible"
+	dc.b	$FC
+	dc.b	"d'éteindre Nurvus de la façon"
+	dc.b	$FD
+	dc.b	"usuelle."
+	dc.b	$FD
+	dc.b	"Je dois me connecter directement"
+	dc.b	$FC
+	dc.b	"dans l'unité centrale de Nurvus"
+	dc.b	$FD
+	dc.b	"et pirater sa sécurité."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Te connecter directement?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"Exact. Je dois vous dire adieu,"
+	dc.b	$FC
+	dc.b	"Chaz."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Demi!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"Adieu à vous tous..."
+	dc.b	$FC
+	dc.b	"Merci pour toute votre aide."
+	dc.b	$FD
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"Accès au système principal..."
+	dc.b	$FD
+	dc.b	"Emission de l'interruption de"
+	dc.b	$FC
+	dc.b	"priorité supérieure..."
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Est-ce...est-ce terminé?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"L'opération est une réussite."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Demi!"
+	dc.b	$FC
+	dc.b	"Où es tu, maintenant?"
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"Je suis située dans l'unité"
+	dc.b	$FC
+	dc.b	"centrale de Nurvus."
+	dc.b	$FD
+	dc.b	"Actuellement, tous les systèmes"
+	dc.b	$FC
+	dc.b	"de maintenance environnementale"
+	dc.b	$FD
+	dc.b	"de Motavia sont éteints."
+	dc.b	$FD
+	dc.b	"Les désastres divers ont cessé."
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$0071
+	dc.b	"Mais tôt ou tard, en l'absence"
+	dc.b	$FC
+	dc.b	"de ces systèmes de maintenance"
+	dc.b	$FD
+	dc.b	"environnementale, la planète"
+	dc.b	$FC
+	dc.b	"entière deviendra dépourvue de"
+	dc.b	$FD
+	dc.b	"toute vie."
+	dc.b	$FD
+	dc.b	"Nous devons rétablir les"
+	dc.b	$FC
+	dc.b	"fonctionnalités de chaque"
+	dc.b	$FD
+	dc.b	"système le plus rapidement"
+	dc.b	$FC
+	dc.b	"possible!"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Pour accomplir cela, nous devons"
+	dc.b	$FC
+	dc.b	"nous attaquer au problème Zelan."
+	dc.b	$FD
+	dc.b	$F2, $01
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"C'est exact. Aussi longtemps que"
+	dc.b	$FC
+	dc.b	"ce satellite envoie des signaux"
+	dc.b	$FD
+	dc.b	"de commande erronés, le contrôle"
+	dc.b	$FC
+	dc.b	"des systèmes nous échappera."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Chaz. Pourquoi ne pas nous en"
+	dc.b	$FC
+	dc.b	"charger?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"...D'accord!"
+	dc.b	$FC
+	dc.b	"Direction Zelan!"
+	dc.b	$FD
+	dc.b	$F2, $00
+	dc.w	$004F
+	dc.b	$F4
+	dc.b	$05
+	dc.b	"Mais, Zelan n'est-il pas situé"
+	dc.b	$FC
+	dc.b	"dans l'espace?"
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"Soyez sans craintes. Il y a une"
+	dc.b	$FC
+	dc.b	"navette de secours dans Nurvus."
+	dc.b	$FD
+	dc.b	"Elle est en préparation en"
+	dc.b	$FC
+	dc.b	"prévision de votre déplacement."
+	dc.b	$FD
+	dc.b	"Malheureusement, je ne pourrai"
+	dc.b	$FC
+	dc.b	"vous accompagner."
+	dc.b	$FD
+	dc.b	"Je dois demeurer ici et"
+	dc.b	$FC
+	dc.b	"m'assurer du bon fonctionnement"
+	dc.b	$FD
+	dc.b	"de chaque système."
+	dc.b	$FD
+	dc.b	"Une fois sur Zelan, vous verrez"
+	dc.b	$FC
+	dc.b	$F2, $00
+	dc.w	$0072
+	dc.b	"mon maître, Wren."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$06
+	dc.b	"Wren? Je le connais!"
+	dc.b	$FD
+	dc.b	"Quand j'étais au Bio-labo, j'ai"
+	dc.b	$FC
+	dc.b	"souvent communiqué avec lui!"
+	dc.b	$FD
+	dc.b	"Par contre, depuis il y a"
+	dc.b	$FC
+	dc.b	"environ six mois, j'ai été"
+	dc.b	$FD
+	dc.b	"incapable de le joindre."
+	dc.b	$FC
+	dc.b	"Je suis inquiète à son sujet!"
+	dc.b	$FD
+	dc.b	"Je t'en prie, Chaz, allons"
+	dc.b	$FC
+	dc.b	"trouver Wren sur Zelan..."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$07
+	dc.b	"Alors, bonne chance sur Zelan"
+	dc.b	$FC
+	dc.b	"à vous tous..."
+	dc.b	$FD
+	dc.b	"Soyez prudents."
+	dc.b	$FD
+	dc.b	$F7
+
+	dc.b	$F4
+	dc.b	$05
+	dc.b	"Chaz...Désolé, mais..."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$01
+	dc.b	"Je sais, je sais. Tu ne peux pas"
+	dc.b	$FC
+	dc.b	"laisser ta petite soeur."
+	dc.b	$FD
+	dc.b	$F4
+	dc.b	$05
+	dc.b	"Oui, c'est ça. Désolé."
+	dc.b	$FF
+
+; $1
+	dc.b	$FF
+
+; $2
+	dc.b	$FF
+
+; $3
+	dc.b	$FF
+
+; $4
+	dc.b	$FF
+
+; $5
+	dc.b	$FF
+
+; $6
+	dc.b	$FF
+
+; $7
+	dc.b	$FF
+
+; $8
+	dc.b	$FF
+
+; $9
+	dc.b	$FF
+
+; $A
+	dc.b	$FF
+
+; $B
+	dc.b	$F4
+	dc.b	$13
+	dc.b	"Tiens, tiens..."
+	dc.b	$FD
+	dc.b	"Vous avez réussi à vous rendre"
+	dc.b	$FC
+	dc.b	"jusqu'ici...tel que prévu."
+	dc.b	$FD
+	dc.b	"Vous devez savoir que si vous"
+	dc.b	$FC
+	dc.b	"faites un seul pas vers moi,"
+	dc.b	$FD
+	dc.b	"ce sera à votre perte."
+	dc.b	$FD
+	dc.b	"Je ne vous laisserai pas vous"
+	dc.b	$FC
+	dc.b	"enfuir deux fois!"
+	dc.b	$FD
+	dc.b	"Oh, entité adorée!"
+	dc.b	$FC
+	dc.b	"Prête-moi ta force!"
+	dc.b	$FF
